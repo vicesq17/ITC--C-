6d8d4160a9c882f7ae91fdd65c23d58caf827e09
vicesq17 <a01708849@tec.mx>
2024-11-23 11:09:44 -0600

    Update ingresos.h

--- ingresos.h
+++ ingresos.h
@@ -1,3 +1,5 @@
+#ifndef INGRESOS
+#define INGRESOS
 #include <iostream>
 class Ingresos{
 private:
@@ -7,50 +9,13 @@ private:
 
 public: 
     Ingresos();
-    Ingresos(float cl, float re, float bo) : cliente(cl),retorno(re),bono(bo){};
-    
+    Ingresos(float cl, float re, float bo);
     float get_cliente();
     float get_retorno();
     float get_bono();
-    
     void set_cliente(float);
     void set_retorno(float);
     void set_bono(float);
-
     void operabilidad();
-
-
 };
-
-Ingresos::Ingresos(){
-    cliente=0.0;
-    retorno=0.0;
-    bono=0.0;
-}
-
-float Ingresos::get_cliente(){
-    return cliente;
-}
-float Ingresos::get_retorno(){
-    return retorno;
-}
-float Ingresos::get_bono(){
-    return bono;
-}
-
-void Ingresos::set_cliente(float cl){
-    cliente=cl;
-
-}
-void Ingresos::set_retorno(float re){
-    retorno=re;
-}
-void Ingresos::set_bono(float bo){
-    bono=bo;
-}
-
-void Ingresos::operabilidad(){
-    bono=cliente*retorno;
-
-}
-
+#endif 
