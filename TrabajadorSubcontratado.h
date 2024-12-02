d72476e554444340826b7dfebf14006ea7e39d13
vicesq17 <a01708849@tec.mx>
2024-12-02 00:03:39 -0600

    Update TrabajadorSubcontratado.h

--- TrabajadorSubcontratado.h
+++ TrabajadorSubcontratado.h
@@ -1,36 +1,32 @@
+
 #ifndef TRABAJADOR_SUBCONTRATADO_H
 #define TRABAJADOR_SUBCONTRATADO_H
 
 #include "Trabajador.h"
 
-class Cont: public Trabajador {
-private:
-    float comision;
-
-public:
-    Cont(){
-        nombre = "";
-        salario = 0.0;
-        comision = 0.0;
-    }
-    Cont(std::string nom, float sal, float com){
-        nombre = nom;
-        salario = sal;
-        comision = com;
-    }
-
-    float get_comision(){
-        return comision;
-    }
-
-    void set_comision(float num){
-        comision = num;
-    }
-
-    float calcula(){
-        salario = salario + comision;
-    }
-
+class TrabajadorSubcontratado: public Trabajador {
+    private:
+        float comision;
+    public:
+        TrabajadorSubcontratado(){
+            nombre = "";
+            salario = 0.0;
+            comision = 0.0;
+        }
+        TrabajadorSubcontratado(std::string nom, float sal, float com){
+            nombre = nom;
+            salario = sal;
+            comision = com;
+        }
+        float get_comision(){
+            return comision;
+        }
+        void set_comision(float num){
+            comision = num;
+        }
+        float calcula(){
+            salario = salario + comision;
+            return salario;
+        }
 };
-
 #endif
